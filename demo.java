26d225483b016270ff77d52f1553c12362c3a513
SitUjjwal <ujjwal@example.com>
2026-07-13 06:41:10 +0530

    Initial Java DSA commit

---
+++ demo.java
@@ -0,0 +1,12 @@
+public class demo {
+    public static void main(String[] args) {
+       /*  int age = 25;
+        String name = "John";
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age); */
+
+        int age; // Declare a variable to store the age  
+                  age=25;//reinstantiation
+        System.out.println(age); // Print the age
+    }   
+}
