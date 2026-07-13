e3f374cd82a9562f2b64daa5f1b5b1a07571c182
SitUjjwal <ujjwal@example.com>
2026-07-13 21:43:41 +0530

    Add third Java examples

---
+++ third/sec.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class sec {  
+    public static void main(String[] args) {  
+        Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter your age: ");
+        // int age = sc.nextInt();
+        // System.out.println("You entered: " + age);
+        // System.out.println("Enter your name: ");
+        // String s=sc.nextLine(); // consume the newline character
+        
+        // System.out.println("You entered: " + s);
+
+        char ch = sc.next().charAt(0);
+        System.out.println("You entered: " + ch);
+    }
+    
+}
