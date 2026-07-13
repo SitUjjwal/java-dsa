335421495b785ecc85835bf0f9b0cd08efb3097f
Ujjwal Sharan <ujjwalsharan82@gmail.com>
2026-07-13 06:34:08 +0530

    Add files via upload

---
+++ sum01.java
@@ -0,0 +1,8 @@
+public class sum01 {
+    public static void main(String[] args) {
+        int num1 = 5; // Declare and initialize the first number
+        int num2 = 7; // Declare and initialize the second number
+        
+        System.out.println(num1+num2); // Print the result
+    }
+}
